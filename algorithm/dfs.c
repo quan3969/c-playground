d12353d85bcc9cd090aa7758bf65832faae11685
Jason Zheng <quan3969@qq.com>
2023-06-17 18:33:02 +0800

    Optimize DFS

--- algorithm/dfs.c
+++ algorithm/dfs.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_SIZE 1000
@@ -13,63 +12,49 @@ typedef struct {
     int top;
 } Stack;
 
-Node* create_node(int v)
-{
+Node* create_node(int v) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->vertex = v;
     node->next = NULL;
     return node;
 }
 
-void add_edge(Node* graph[], int d, int s)
-{
+void add_edge(Node* graph[], int d, int s) {
     Node* node = create_node(s);
     node->next = graph[d];
     graph[d] = node;
+
     node = create_node(d);
     node->next = graph[s];
     graph[s] = node;
 }
 
-Stack* init_stack()
-{
+Stack* init_stack() {
     Stack* stack = (Stack*)malloc(sizeof(Stack));
     stack->top = -1;
     return stack;
 }
 
-int is_empty(Stack* stack)
-{
+int is_empty(Stack* stack) {
     return (stack->top == -1) ? 1 : 0;
 }
 
-void push(Stack* stack, int item)
-{
-    if (stack->top == MAX_SIZE - 1)
+void push(Stack* stack, int item) {
+    if (stack->top == MAX_SIZE - 1) {
         return;
-    else {
-        if (stack->top == -1)
-            stack->top = 0;
-        stack->items[++stack->top] = item;
     }
+    stack->items[++stack->top] = item;
 }
 
-int pop(Stack* stack)
-{
-    int item;
-    if (is_empty(stack))
-        item = -1;
-    else {
-        item = stack->items[stack->top--];
+int pop(Stack* stack) {
+    if (is_empty(stack)) {
+        return -1;
     }
-    return item;
+    return stack->items[stack->top--];
 }
 
-void dfs(Node* graph[], int start, int n)
-{
-    unsigned char* visited = (unsigned char*)malloc(sizeof(unsigned char) * n);
-    for (int i = 0; i < n; i++)
-        visited[i] = 0;
+void dfs(Node* graph[], int start, int n) {
+    unsigned char* visited = (unsigned char*)calloc(n, sizeof(unsigned char));
     
     Stack* stack = init_stack();
     push(stack, start);
@@ -89,13 +74,18 @@ void dfs(Node* graph[], int start, int n)
             temp = temp->next;
         }
     }
+
+    free(visited);
+    free(stack);
 }
 
-int main()
-{
-    Node* graph[6];
-    for (int i = 0; i < 6; i++)
+int main() {
+    int num_vertices = 6;
+    Node* graph[num_vertices];
+
+    for (int i = 0; i < num_vertices; i++) {
         graph[i] = NULL;
+    }
 
     add_edge(graph, 0, 1);
     add_edge(graph, 0, 2);
@@ -105,8 +95,17 @@ int main()
     add_edge(graph, 3, 4);
     add_edge(graph, 3, 5);
 
-    dfs(graph, 2, 6);
+    dfs(graph, 2, num_vertices);
+
+    for (int i = 0; i < num_vertices; i++) {
+        Node* temp = graph[i];
+        Node* prev;
+        while (temp != NULL) {
+            prev = temp;
+            temp = temp->next;
+            free(prev);
+        }
+    }
 
     return 0;
-    
 }
