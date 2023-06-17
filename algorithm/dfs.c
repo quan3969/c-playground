a8d41a980d5a8ae30188ee0d197c99134c127b40
Jason Zheng <quan3969@qq.com>
2023-06-17 18:22:39 +0800

    Add DFS algorithm

---
+++ algorithm/dfs.c
@@ -0,0 +1,112 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 1000
+
+typedef struct node {
+    int vertex;
+    struct node* next;
+} Node;
+
+typedef struct {
+    int items[MAX_SIZE];
+    int top;
+} Stack;
+
+Node* create_node(int v)
+{
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->vertex = v;
+    node->next = NULL;
+    return node;
+}
+
+void add_edge(Node* graph[], int d, int s)
+{
+    Node* node = create_node(s);
+    node->next = graph[d];
+    graph[d] = node;
+    node = create_node(d);
+    node->next = graph[s];
+    graph[s] = node;
+}
+
+Stack* init_stack()
+{
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    stack->top = -1;
+    return stack;
+}
+
+int is_empty(Stack* stack)
+{
+    return (stack->top == -1) ? 1 : 0;
+}
+
+void push(Stack* stack, int item)
+{
+    if (stack->top == MAX_SIZE - 1)
+        return;
+    else {
+        if (stack->top == -1)
+            stack->top = 0;
+        stack->items[++stack->top] = item;
+    }
+}
+
+int pop(Stack* stack)
+{
+    int item;
+    if (is_empty(stack))
+        item = -1;
+    else {
+        item = stack->items[stack->top--];
+    }
+    return item;
+}
+
+void dfs(Node* graph[], int start, int n)
+{
+    unsigned char* visited = (unsigned char*)malloc(sizeof(unsigned char) * n);
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+    
+    Stack* stack = init_stack();
+    push(stack, start);
+    visited[start] = 1;
+
+    while (!is_empty(stack)) {
+        int currentVertex = pop(stack);
+        printf("%d ", currentVertex);
+
+        Node* temp = graph[currentVertex];
+        while (temp != NULL) {
+            int adjVertex = temp->vertex;
+            if (visited[adjVertex] == 0) {
+                visited[adjVertex] = 1;
+                push(stack, adjVertex);
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main()
+{
+    Node* graph[6];
+    for (int i = 0; i < 6; i++)
+        graph[i] = NULL;
+
+    add_edge(graph, 0, 1);
+    add_edge(graph, 0, 2);
+    add_edge(graph, 1, 3);
+    add_edge(graph, 1, 4);
+    add_edge(graph, 2, 5);
+    add_edge(graph, 3, 4);
+    add_edge(graph, 3, 5);
+
+    dfs(graph, 2, 6);
+
+    return 0;
+    
+}
