77bacffd206967c3afa90f8b68d306e1fa8279c3
Jason Zheng <quan3969@qq.com>
2023-07-19 08:12:30 +0800

    Update move zeroes

--- algorithm/move_zeroes.c
+++ algorithm/move_zeroes.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 void move_zeroes(int arr[], int size) {
-    int i = 0, j = 0;
+    int i = 0;
+    int j = 0;
     while (j < size) {
         if (arr[j] != 0) {
             int temp = arr[i];
@@ -15,8 +16,37 @@ void move_zeroes(int arr[], int size) {
     }
 }
 
+void remove_item(int arr[], int size, int item) {
+    int i = 0;
+    int j = 0;
+    while (j < size) {
+        if (arr[j] != item) {
+            arr[i] = arr[j];
+            i++;
+        }
+        j++;
+    }
+}
+
+void revert_array(int arr[], int size) {
+    int i = 0;
+    int j = size - 1;
+    while (i < j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
+
 int main() {
-    int nums[5] = {0, 23, 0, 2, 123};
-    move_zeroes(nums, 5);
+    int arr1[5] = {0, 23, 0, 2, 123};
+    int arr2[5] = {0, 23, 0, 2, 123};
+    int arr3[5] = {0, 23, 0, 2, 123};
+    move_zeroes(arr1, 5);
+    remove_item(arr2, 5, 2);
+    revert_array(arr3, 5);
+
     return 0;
 }
